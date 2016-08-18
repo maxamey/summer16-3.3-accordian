ba0df850a307d2bfe58825f23dc22fa6d506206e
Max Amey <maxwellamey@gmail.com>
2016-08-17 21:28:01 -0500

    Made some detailed style changes to match the example. Normal mode done, but text from the last panel stil overflows for about a half second when toggled off.

--- javascripts/main.js
+++ javascripts/main.js
@@ -3,5 +3,11 @@
 document.addEventListener("DOMContentLoaded", function(){
   var $bodyElement = $("[data-js=body]");
   var $headingElement = $bodyElement.find(".heading");
-  
+  var $paragraphElement = $bodyElement.find(".paragraph");
+
+  $headingElement.on("click", function(e){
+    var $paragraphOpen = $(e.currentTarget);
+    $paragraphElement.removeClass("active");
+    $paragraphOpen.next().toggleClass("active");
+  });
 });
